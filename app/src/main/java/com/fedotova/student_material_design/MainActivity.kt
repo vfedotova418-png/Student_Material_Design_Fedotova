4acedb8fcb2f55f274132d4cf5587f11748f5afb
vfedotova418-png <vfedotova418@gmail.com>
2026-03-06 09:20:11 +0300

    Step 5: Basic UI completed

--- app/src/main/java/com/fedotova/student_material_design/MainActivity.kt
+++ app/src/main/java/com/fedotova/student_material_design/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.fedotova.student_material_design
 
+import com.fedotova.student_material_design.data.DataSource
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.fedotova.student_material_design.data.Student
 import com.fedotova.student_material_design.ui.theme.Student_Material_DesignTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +34,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Student_Material_DesignTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    StudentApp()
                 }
             }
         }
@@ -31,17 +45,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun StudentIcon(
+    @DrawableRes studentIcon: Int,
+    modifier: Modifier = Modifier
+) {
+    Image(
         modifier = modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_small)),
+        painter = painterResource(studentIcon),
+        contentDescription = null
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Student_Material_DesignTheme {
-        Greeting("Android")
+fun StudentInformation(
+    @StringRes studentName: Int,
+    studentAge: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(studentName),
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small)),
+            style = MaterialTheme.typography.displayMedium
+        )
+        Text(
+            text = stringResource(R.string.years_old, studentAge),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+fun StudentItem(
+    student: Student,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_small))
+    ) {
+        StudentIcon(student.imageResourceId)
+        StudentInformation(student.name, student.age)
+    }
+}
+
+@Composable
+fun StudentApp() {
+    LazyColumn {
+        items(DataSource().students) {
+            StudentItem(student = it)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun StudentPreview() {
+    Student_Material_DesignTheme(darkTheme = false) {
+        StudentApp()
     }
 }
---
+++ app/src/main/java/com/fedotova/student_material_design/data/DataSource.kt
@@ -0,0 +1,59 @@
+package com.fedotova.student_material_design.data
+
+import com.fedotova.student_material_design.R
+
+
+class DataSource {
+    val students = listOf(
+        Student(
+            R.drawable.student1,
+            R.string.student1, 25,
+            R.string.student1_desc
+        ),
+        Student(
+            R.drawable.student2,
+            R.string.student2, 27,
+            R.string.student2_desc
+        ),
+        Student(
+            R.drawable.student3,
+            R.string.student3, 16,
+            R.string.student3_desc
+        ),
+        Student(
+            R.drawable.student4,
+            R.string.student4, 27,
+            R.string.student4_desc
+        ),
+        Student(
+            R.drawable.student5,
+            R.string.student5, 21,
+            R.string.student5_desc
+        ),
+        Student(
+            R.drawable.student6,
+            R.string.student6, 23,
+            R.string.student6_desc
+        ),
+        Student(
+            R.drawable.student7,
+            R.string.student7, 24,
+            R.string.student7_desc
+        ),
+        Student(
+            R.drawable.student8,
+            R.string.student8, 22,
+            R.string.student8_desc
+        ),
+        Student(
+            R.drawable.student9,
+            R.string.student9, 18,
+            R.string.student9_desc
+        ),
+        Student(
+            R.drawable.student10,
+            R.string.student10, 18,
+            R.string.student10_desc
+        )
+    )
+}
