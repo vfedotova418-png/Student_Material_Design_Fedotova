36754be5697eb3063bbf38690fb1366c8c4da2e8
vfedotova418-png <vfedotova418@gmail.com>
2026-03-06 09:29:12 +0300

    Step 6: Custom color scheme added

--- app/src/main/java/com/fedotova/student_material_design/MainActivity.kt
+++ app/src/main/java/com/fedotova/student_material_design/MainActivity.kt
@@ -16,7 +16,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -82,21 +84,29 @@ fun StudentItem(
     student: Student,
     modifier: Modifier = Modifier
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(R.dimen.padding_small))
-    ) {
-        StudentIcon(student.imageResourceId)
-        StudentInformation(student.name, student.age)
+    Card(modifier = modifier) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_small))
+        ) {
+            StudentIcon(student.imageResourceId)
+            StudentInformation(student.name, student.age)
+        }
     }
 }
 
 @Composable
 fun StudentApp() {
-    LazyColumn {
-        items(DataSource().students) {
-            StudentItem(student = it)
+    Scaffold { it ->
+        LazyColumn(contentPadding = it) {
+            items(DataSource().students) {
+                StudentItem(
+                    student = it,
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
         }
     }
 }
