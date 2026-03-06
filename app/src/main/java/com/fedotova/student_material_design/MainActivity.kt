fc27219990b56d677bd163f2b3130e3e3ecc84fb
vfedotova418-png <vfedotova418@gmail.com>
2026-03-06 09:34:52 +0300

    Step 7: Dark theme support added

--- app/src/main/java/com/fedotova/student_material_design/MainActivity.kt
+++ app/src/main/java/com/fedotova/student_material_design/MainActivity.kt
@@ -118,3 +118,11 @@ fun StudentPreview() {
         StudentApp()
     }
 }
+
+@Preview
+@Composable
+fun StudentDarkThemePreview() {
+    Student_Material_DesignTheme(darkTheme = true) {
+        StudentApp()
+    }
+}
