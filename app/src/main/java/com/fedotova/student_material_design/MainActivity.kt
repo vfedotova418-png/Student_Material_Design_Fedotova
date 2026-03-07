19791680d7d83ca4facabc8011714afec3962944
vfedotova418-png <vfedotova418@gmail.com>
2026-03-07 14:00:18 +0300

    Step 10: Top App Bar added

--- app/src/main/java/com/fedotova/student_material_design/MainActivity.kt
+++ app/src/main/java/com/fedotova/student_material_design/MainActivity.kt
@@ -17,11 +17,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
@@ -100,7 +103,11 @@ fun StudentItem(
 
 @Composable
 fun StudentApp() {
-    Scaffold { it ->
+    Scaffold(
+        topBar = {
+            StudentTopAppBar()
+        }
+    ) { it ->
         LazyColumn(contentPadding = it) {
             items(DataSource().students) {
                 StudentItem(
@@ -111,6 +118,31 @@ fun StudentApp() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StudentTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_student_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @Preview
